f848e79051d3a356a407cdc727491b769a30e0c2
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-05-27 14:44:58 +0530

    Update Problem 2.py

--- Python/Problem 2.py
+++ Python/Problem 2.py
@@ -2,6 +2,7 @@
 #1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 #By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
+# USING FUNCTION-----------------------------------------------------------#
 def compute():
 	ans = 0
 	x = 1  # Represents the current Fibonacci number being processed
@@ -11,3 +12,16 @@ def compute():
 			ans += x
 		x, y = y, x + y
 	print(ans)
+
+#---------------------------------OR---------------------------------------#
+#                       WITHOUT USING FUNCTION
+ans = 0
+x = 1  # Represents the current Fibonacci number being processed
+y = 2  # Represents the next Fibonacci number in the sequence
+while x <= 4000000:
+	if x % 2 == 0:
+		ans += x
+	t=x+y
+	x=y
+	y=t
+print(ans)
